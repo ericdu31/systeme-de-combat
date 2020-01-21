03d247bc0d0162f2ffea7006746078e972432478
ericdu31 <60115655+ericdu31@users.noreply.github.com>
2020-01-21 22:38:28 +0100

    exo1

--- combat.c
+++ combat.c
@@ -4,5 +4,15 @@ int main(){
 
 printf("hello");
     
+
+//-------------------------------------------------------------//
+
+printf("%d\n",pointDeVie );
+printf("vous attaquez le boss\n" );
+printf("vous lui enlevez 10 pv\n" );
+printf("%d\n",pointDeVie -attaque1 );
+
     return 0;
+
+
 }
